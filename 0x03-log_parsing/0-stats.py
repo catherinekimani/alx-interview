c381a02fb8559583c67c96a94b2e229605445bce
catherinekimani <catherinekimani882@gmail.com>
2024-01-28 23:49:00 +0300

    script that reads stdin line by line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,20 +5,21 @@ if __name__ == '__main__':
     filesize, line_count = 0, 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {code: 0 for code in codes}
-    def print_stats(stats: dict, filesize: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
 
+    def print_stats(stats: dict, filesize: int) -> None:
+        """ script that reads stdin line by line and computes metrics """
+        print("File Size: {:d}".format(filesize))
+        for code, count in sorted(stats.items()):
+            if count:
+                print("{}: {}".format(code, count))
     try:
         for line in sys.stdin:
             line_count += 1
             data = line.split()
             try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
+                statusCode = data[-2]
+                if statusCode in stats:
+                    stats[statusCode] += 1
             except BaseException:
                 pass
             try:
@@ -31,29 +32,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print_stats(stats, filesize)
         raise
-    # def print_stats(stats: dict, file_size: int) -> None:
-    #     """ script that reads stdin line by line and computes metrics """
-    #     print("File Size: {:d}".format(filesize))
-    #     for code, count in sorted(stats.items()):
-    #         if count:
-    #             print("{}: {}".format(code, count))
-    # try:
-    #     for line in sys.stdin:
-    #         line_count += 1
-    #         data = line.split()
-    #         try:
-    #             statusCode = data[-2]
-    #             if statusCode in stats:
-    #                 stats[statusCode] += 1
-    #         except BaseException:
-    #             pass
-    #         try:
-    #             filesize += int(data[-1])
-    #         except BaseException:
-    #             pass
-    #         if line_count % 10 == 0:
-    #             print_stats(stats, filesize)
-    #     print_stats(stats, filesize)
-    # except KeyboardInterrupt:
-    #     print_stats(stats, filesize)
-    #     raise
